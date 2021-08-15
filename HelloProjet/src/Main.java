5fb4b30e1b6d915561e25e43ba69e070765efafe
zjc <zjc961204@163.com>
2021-08-15 21:25:24 +0800

    制造冲突

--- HelloProjet/src/Main.java
+++ HelloProjet/src/Main.java
@@ -6,6 +6,9 @@ public class Main {
     public void testHelloWrold() {
         System.out.println("HelloWorld!");
 		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
+		System.out.println("HelloWorld!");
 		//123
     }
 }
